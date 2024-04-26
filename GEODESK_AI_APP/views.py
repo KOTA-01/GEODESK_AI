9ea14250310156aa2ef493367f63edbe301365be
Jamieson Ritchie <jamiesonritchie@gmail.com>
2024-04-26 15:37:40 +1000

    made a home page html template that can be edited :)

--- GEODESK_AI_APP/views.py
+++ GEODESK_AI_APP/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Welcome to My Django Web Page!</h1>")
+    return render(request, 'GEODESK_AI_APP/home.html')
