baea04950b349211ab299e54b8ba207173cdaeab
Jamieson Ritchie <jamiesonritchie@gmail.com>
2024-04-26 15:32:32 +1000

    created a working webpage on local server that can be run

--- GEODESK_AI/urls.py
+++ GEODESK_AI/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('GEODESK_AI_APP.urls')),  # Includes the myapp URLs in the project
 ]
