883bc8204a8f855b0ea213d59fc565c786cd00a3
KOTA-01 <j.bird02@outlook.com>
2024-04-27 22:13:21 +1000

    more stuff

--- GEODESK_AI_APP/urls.py
+++ GEODESK_AI_APP/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Directs root URL of the app to the home view
+    path('home/', views.home, name='home'),
     path('umap/', views.umap_view, name='umap'), # Umap root
 ]
--- GEODESK_AI_APP/views.py
+++ GEODESK_AI_APP/views.py
@@ -10,15 +10,5 @@ def home(request):
     return render(request, 'GEODESK_AI_APP/home.html')
 
 def umap_view(request):
-    # Generate random data points for demonstration
-    np.random.seed(42)
-    n_samples = 1000
-    n_features = 10
-    data = np.random.rand(n_samples, n_features)
-
-    # Process the data using UMAP
-    reducer = umap.UMAP()
-    embedding = reducer.fit_transform(data)
-
     # Pass the embedding data to the template
-    return render(request, 'umap.html', {'embedding': embedding})
+    return render(request, 'GEODESK_AI_APP/umap.html')
