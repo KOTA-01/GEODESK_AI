0b50c92b0aa9f3c1e601ef9d0f560bcea3a94b6f
KOTA-01 <j.bird02@outlook.com>
2024-04-27 21:49:11 +1000

    stuff so far

--- GEODESK_AI_APP/urls.py
+++ GEODESK_AI_APP/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Directs root URL of the app to the home view
+    path('umap/', views.umap_view, name='umap'), # Umap root
 ]
